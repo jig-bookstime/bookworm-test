aa8791f13e12dd3aabaeedc3b0257a16a0e3d939
jig-bookstime <jigyansu.nanda@bookstime.com>
2024-12-09 11:48:50 +0530

    basic bot changes

--- src/app/messages.js
+++
@@ -1,8 +0,0 @@
-async function resetMessage(context, state) {
-    state.deleteConversationState();
-    await context.sendActivity("Ok lets start this over.");
-}
-
-module.exports = {
-    resetMessage,
-};
