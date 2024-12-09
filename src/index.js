e2c876360c49d3b2a764c5812007620bf76c4592
jig-bookstime <jigyansu.nanda@bookstime.com>
2024-12-09 08:01:12 +0530

    env var checked server working

--- src/index.js
+++ src/index.js
@@ -14,13 +14,6 @@ server.listen(process.env.port || process.env.PORT || 3978, () => {
 server.get("/", async (req, res) => {
     res.json({
         message: "success",
-        MicrosoftAppId: process.env.BOT_ID,
-        MicrosoftAppType: process.env.BOT_TYPE,
-        MicrosoftAppTenantId: process.env.BOT_TENANT_ID,
-        MicrosoftAppPassword: process.env.BOT_PASSWORD,
-        openAIKey: process.env.OPENAI_API_KEY,
-        openAIAssistantId: process.env.OPENAI_ASSISTANT_ID,
-        port: process.env.PORT,
     });
 });
 
